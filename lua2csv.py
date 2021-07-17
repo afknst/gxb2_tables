09c2ea81fe5b62a1cb23c4307d2da66713ae7877
afknst <k.afknst@gmail.com>
2021-07-17 21:45:17 +0200

    remove log

--- lua2csv.py
+++ lua2csv.py
@@ -1,15 +1,11 @@
 import re
 import json
 import csv
-import sys
-import logging
-from datetime import datetime
 from pathlib import Path
 
 import luadata
 import pandas as pd
 
-VERSION = "Jul_17_2021"
 MAIN_DIR = Path("../src/data/tables/")
 TO_COMBINE = [
     "dropbox",
@@ -21,17 +17,6 @@ TO_COMBINE = [
     "skill",
     "sound",
 ]
-NOW = datetime.now()
-
-LOGGER = logging.getLogger()
-LOGGER.setLevel(logging.DEBUG)
-LOGGER.addHandler(logging.FileHandler(f"{VERSION}.log"))
-LOGGER.addHandler(logging.StreamHandler(sys.stdout))
-
-
-def log_and_print(*_msg):
-    _to_log = ' '.join([str(_) for _ in _msg])
-    LOGGER.debug(_to_log)
 
 
 def save2csv(_file_path, _dict):
@@ -41,8 +26,8 @@ def save2csv(_file_path, _dict):
     for _i in range(1, _nk_real + 1):
         if _i not in _rk:
             _rk[_i] = f"unknown_{_i}"
-            log_and_print(f"[KEYERROR] {_file_path}")
-            log_and_print(f"[ADDED COLUMN {_i}] {_rk[_i]}")
+            print(f"[KEYERROR] {_file_path}")
+            print(f"[ADDED COLUMN {_i}] {_rk[_i]}")
 
     _keys = [_rk[_i + 1] for _i in range(_nk_real)]
     with open(_file_path, 'w') as _file:
@@ -60,14 +45,6 @@ def save2json(_file_path, _dict):
 
 
 def lua_convert(_file_path, _out=".csv"):
-    _str_file_path = str(_file_path).lower()
-
-    if "activity" in _str_file_path:
-        _mtime = datetime.fromtimestamp(_file_path.stat().st_mtime)
-        if (NOW - _mtime).days > 13:
-            log_and_print(f"[IGNORED] {_file_path}")
-            return
-
     _out = _out.lower()
     assert _out in [".csv", ".json"]
 
@@ -86,7 +63,7 @@ def lua_convert(_file_path, _out=".csv"):
     with open(_file_path, 'r') as _file:
         _data = _file.read()
         if " = import(" in _data:
-            log_and_print(f"[IGNORED] {_file_path}")
+            print(f"[IGNORED] {_file_path}")
             return
 
         _data = _data.replace('return table', '')
@@ -101,9 +78,9 @@ def lua_convert(_file_path, _out=".csv"):
         if "keys" in _dict and "rows" in _dict and len(
                 _dict["keys"]) > 0 and len(_dict["rows"]) > 0:
             _converter(_dist, _dict)
-            log_and_print(f"[CONVERTED] {_file_path}")
+            print(f"[CONVERTED] {_file_path}")
         else:
-            log_and_print(f"[IGNORED] {_file_path}")
+            print(f"[IGNORED] {_file_path}")
 
 
 if __name__ == "__main__":
@@ -118,11 +95,11 @@ if __name__ == "__main__":
 
     for _t in TO_COMBINE:
         CSV_FILES = sorted(TABLES.glob(f'./{_t}?.csv'))
-        log_and_print("[TO COMBINE]", *CSV_FILES)
+        print("[TO COMBINE]", *CSV_FILES)
 
         COMBINED_CSV = pd.concat([pd.read_csv(_) for _ in CSV_FILES])
         COMBINED_CSV.sort_values("id", inplace=True)
         COMBINED_CSV.set_index("id", inplace=True)
         COMBINED_CSV.to_csv(COMBINED_TABLES / f"{_t}.csv")
 
-        log_and_print("[COMBINED]", COMBINED_TABLES / f"{_t}.csv")
+        print("[COMBINED]", COMBINED_TABLES / f"{_t}.csv")
